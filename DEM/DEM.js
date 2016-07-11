6f7b60678083d3131f6360f2fdce09ae6f75a6b0
KjellTech <andreas_w_paulsen@yahoo.com>
2016-07-11 15:34:34 +0200

    Closer zooming.

--- DEM/DEM.js
+++ DEM/DEM.js
@@ -36,7 +36,7 @@ function changeGeometry() {
 
 function init() {
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.set(0, -2, 5);
     camera.lookAt(0, 0, 0);
 
